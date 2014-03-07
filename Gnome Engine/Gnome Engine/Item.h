b673304abe55e877172a72bbf3e1d417f76b409b
pfeeney <p.feeney@wpi.edu>
2014-03-07 14:16:38 -0500

    Update Item.h

--- Gnome Engine/Gnome Engine/Item.h
+++ Gnome Engine/Gnome Engine/Item.h
@@ -1,8 +1,19 @@
 #pragma once
+
+enum WEAPON_TYPE {SWORD, AXE, LANCE, BOW, ANIMA, LIGHT, DARK, STAFF, ITEM};
+
 class Item
 {
 public:
-	Item(void);
+	Item(WEAPON_TYPE _type, int _rank, int might, int _accuracy, int _crit, int _min_range, int _max_range);
 	~Item(void);
+
+	WEAPON_TYPE type;
+	int rank;
+	int might;
+	int accuracy;
+	int crit;
+	int min_range;
+	int max_range;
 };
 
