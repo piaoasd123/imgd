502e8cbe10a7301ed1e80573191a9b4d35dd271c
pfeeney <p.feeney@wpi.edu>
2014-03-07 14:16:17 -0500

    miscellaneous refactoring

--- Gnome Engine/Gnome Engine/Item.cpp
+++ Gnome Engine/Gnome Engine/Item.cpp
@@ -2,8 +2,15 @@
 #include "Item.h"
 
 
-Item::Item(void)
+Item::Item(WEAPON_TYPE _type, int _rank, int _might, int _accuracy, int _crit, int _min_range, int _max_range)
 {
+	type = _type;
+	rank = _rank;
+	might = _might;
+	accuracy = _accuracy;
+	crit = _crit;
+	min_range = _min_range;
+	max_range = _max_range;
 }
 
 
